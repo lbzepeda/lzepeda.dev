333d16062142702dd3eab6cb3d26bb31df0ddca0
lbzepeda <levi.zepeda06@gmail.com>
2025-02-10 08:56:00 -0600

    refactor(about): small impruvements

--- src/components/about/About.svelte
+++ src/components/about/About.svelte
@@ -4,7 +4,7 @@
 
 <section
   id="about"
-  class="relative min-h-screen w-full flex items-center justify-center bg-gray-100 dark:bg-indigo-900">
+  class="relative min-h-screen w-full flex items-center justify-center bg-gray-100 dark:bg-indigo-900 overflow-y-hidden">
   <div
     class="w-full max-w-[1100px] px-4 sm:px-6 lg:px-8 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
     <AboutContent />
